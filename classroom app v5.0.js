6a963b12326162a2a7854f26f59e7a69140a418c
grmkng <168589585+grmkng@users.noreply.github.com>
2024-08-25 22:03:25 +0100

    classroom app v5.0

---
+++ classroom app v5.0.js
@@ -0,0 +1,58 @@
+/**
+ * @param {array} grades
+ * @param {number} grade
+ */
+export function addGrade(grades, grade) {
+    grades.push(grade)
+    return grades
+}
+
+/**
+ * @param {array} grades
+ */
+export function getNumberOfGrades(grades) {
+    return grades.length
+}
+
+/**
+ * @param {array} grades
+ */
+export function getFirstGrade(grades) {
+    return grades[0]
+}
+
+/**
+ * @param {array} grades
+ */
+export function getLastGrade(grades) {
+    return grades[grades.length - 1]
+}
+
+/**
+ * @param {array} grades
+ */
+export function getSumOfGrades(grades) {
+    let sum = grades.reduce(function (total, current){
+        return total + current
+    }, 0)
+    return sum
+}
+
+/**
+ * @param {array} grades
+ */
+export function getAverageGrade(grades) {
+    let sum = grades.reduce(function (total, current){
+        return total + current
+    }, 0)
+    return sum / grades.length
+}
+
+/**
+ * @param {array} grades
+ */
+export function getRaisedGrades(grades) {
+    return grades.map(function (grade){
+    return grade + 2
+    })
+}
